1187c0cdd31a49b53753f949eaa62084cd1a07c1
Tinschry <Christine.xhzhao@hotmail.com>
2023-11-29 11:30:01 +0000

    File transfer

--- main.py
+++ main.py
@@ -77,6 +77,3 @@ indices = (nll_list.argsort()[:6])  # extra indices of the three points around e
 indices = np.sort(indices)
 print(indices)
 
-def Cal_x3(x0,x1,x2,y0,y1,y2):
-    x3=((x2**2-x1**2)*y0+(x0**2-x2**2)*y1+(x1**2-x0**2)*y2)/((x2-x1)*y0+(x0-x2)*y1+(x1-x0)*y2)/2
-    return x3
---
+++ minimiser.py
@@ -0,0 +1,8 @@
+"""
+Minimisers
+"""
+
+
+def parabolic_1d(init_x, init_y):
+    
+    return 0
